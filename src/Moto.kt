574d629b1d23665fbd8af90e6bd022ac94295899
blangetti1 <santinoblangetti23@gmail.com>
2024-10-15 22:29:23 -0300

    Falta iterar todo

---
+++ src/Moto.kt
@@ -0,0 +1,14 @@
+class Moto(nombre: String, velocidadMaxima: Int, combustible: Int, aceleracion: Int, frenado: Int) :
+    VehiculoCustom(nombre, velocidadMaxima, combustible, aceleracion, frenado) {
+    override fun acelerar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun frenar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun avanzar() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/VehiculoCustom.kt
@@ -0,0 +1,16 @@
+open class VehiculoCustom(
+    nombre: String, velocidadMaxima: Int, combustible: Int, aceleracion: Int, frenado: Int
+
+): Vehiculo(nombre, velocidadMaxima, combustible, aceleracion, frenado) {
+    override fun acelerar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun frenar() {
+        TODO("Not yet implemented")
+    }
+
+    override fun avanzar() {
+        TODO("Not yet implemented")
+    }
+}
