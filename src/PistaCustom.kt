e5d38d0031c49997f8befbff65d79fee4f182b52
blangetti1 <santinoblangetti23@gmail.com>
2024-10-22 22:01:46 -0300

    la etapa 2 para trabajar

---
+++ src/PistaCustom.kt
@@ -0,0 +1,19 @@
+open class PistaCustom (
+    longitud: Int,
+     clima: String,
+    )
+    :Pista(longitud, clima )
+{
+    override fun aplicarCondiciones(vehiculo: Vehiculo)
+    {
+
+    }
+    override fun cambiarClima()
+    {
+
+    }
+    override fun aplicarEventoAleatorio(vehiculo: Vehiculo)
+    {
+
+    }
+}
